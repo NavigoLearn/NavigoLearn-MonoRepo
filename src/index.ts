c17982c2267640e116501b78bd1fac77b5751d56
sopy <doimpt@sopy.one>
2023-04-03 18:17:16 +0300

    Fixed routing for backend

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,7 @@ app.use("/",
 
     router: {
       // when / api goes to http://localhost:3001/api
-      "/api": "http://localhost:3001/api",
+      "/api": "http://localhost:3001/",
     },
   })
 );
