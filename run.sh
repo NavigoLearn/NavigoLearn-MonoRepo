e1e8b048446ac02526ad88f91085a01e5f17d0a3
sopy <doimpt@sopy.one>
2023-05-05 23:15:02 +0300

    fixes and run.sh for server

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+
+# Build the project
+# go to the backend directory
+cd src/api || exit 1
+# build the project
+npm i
+npm run build
+
+# if the build is successful
+if [ $? -eq 1 ]; then
+  echo "Build failed for the backend api"
+  exit 1
+fi
+
+# run the build
+screen -dmS api npm run start
+
+# build the frontend
+cd ../frontend || exit 1
+npm i
+npm run build
+
+# if the build is successful
+if [ $? -eq 1 ]; then
+  echo "Build failed for the frontend"
+  exit 1
+fi
+
+# run the build
+cd dist/server || exit 1
+screen -dmS frontend node entry.mjs
