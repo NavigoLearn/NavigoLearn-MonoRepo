548224da6c8f798e20f31d7ddb781f1c17817515
sopy <doimpt@sopy.one>
2023-05-18 22:02:34 +0300

    building frontend then api to reduce visible downtime fixing let's encrypt challenges

--- run.sh
+++ run.sh
@@ -1,25 +1,8 @@
 #!/usr/bin/bash
 
-
 # Build the project
-# go to the backend directory
-cd src/api || exit 1
-# build the project
-rm -rf dist
-npm i
-npm run build
-
-# if the build is successful
-if [ $? -eq 1 ]; then
-  echo "Build failed for the backend api"
-  exit 1
-fi
-
-# run the build
-screen -dmS api npm run start
-
 # build the frontend
-cd ../frontend || exit 1
+cd src/frontend || exit 1
 rm -rf dist
 npm i
 npm run build
@@ -33,3 +16,19 @@ fi
 # run the build
 cd dist/server || exit 1
 screen -dmS frontend node entry.mjs
+
+# go to the backend directory
+cd ../../../api || exit 1
+# build the project
+rm -rf dist
+npm i
+npm run build
+
+# if the build is successful
+if [ $? -eq 1 ]; then
+  echo "Build failed for the backend api"
+  exit 1
+fi
+
+# run the build
+screen -dmS api npm run start
