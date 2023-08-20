c817b7f8445ad08c9b4cfad51de7ac69f3377e87
sopy <contact@sopy.one>
2023-08-20 04:01:03 +0300

    Adapt frontend restart script for server changes

--- server_conf/webhooks/push/restartFrontend.sh
+++ server_conf/webhooks/push/restartFrontend.sh
@@ -9,10 +9,11 @@ git checkout $branch_name # checkout to the branch
 git pull origin $branch_name # pull the latest code
 npm install # install the dependencies
 npm run build || exit 1 # build the frontend
+cd dist/server || exit 1 # change directory to server folder
 # Get the list of screen sessions
 session_list=$(screen -ls | grep "frontend" | awk '{print $1}')
 # Iterate through the sessions and kill them
 for session_id in $session_list; do
     screen -X -S $session_id quit
 done
-screen -dmS frontend npm run start # start the frontend
+screen -dmS frontend node entry.mjs # start the frontend
