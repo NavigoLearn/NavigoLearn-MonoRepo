a2af389bfaebc465fc7e45588312750130f08f65
sopy <contact@sopy.one>
2023-09-23 16:04:45 +0300

    Simplify and error-proof backend and frontend restart scripts
    This commit refactors the restartBackend.sh and restartFrontend.sh scripts to streamline the process and account for errors. We've introduced a function, change_dir_and_check, to change directories and ensure successful navigation. Before, the scripts would exit if it failed to change directories, possibly leaving processes unfinished. The git commands are now combined into one line for brevity. Killing existing screen sessions has been condensed into one line, removing the need for an iteration. The cache clearing command now uses sudo to ensure permissions aren't an issue. Meanwhile, the start commands at the end of both scripts remained the same.

--- server_conf/webhooks/push/restartBackend.sh
+++ server_conf/webhooks/push/restartBackend.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 
-# define branch name
 branch_name="prod"
 
-cd src/api || exit 1 # change directory to frontend folder
-
+change_dir_and_check() {
+   cd "$1" || {
+      echo "Could not change to directory $1. Exiting...";
+      exit 1;
+   }
+}
 # update environment variables
-cd env || exit 1 # change directory to env folder
-git reset --hard # reset the changes
-git checkout $branch_name # checkout to the branch
-git pull origin $branch_name # pull the latest code
-cd .. # change directory to root folder
+change_dir_and_check src/api/env
+git reset --hard && git checkout $branch_name && git pull origin $branch_name
 
 # Start the backend
-git reset --hard # reset the changes
-git checkout $branch_name # checkout to the branch
-git pull origin $branch_name # pull the latest code
-npm install # install the dependencies
-npm run build || exit 1 # build the frontend
-# Get the list of screen sessions
-session_list=$(screen -ls | grep "api" | awk '{print $1}')
-# Iterate through the sessions and kill them
-for session_id in $session_list; do
-    screen -X -S $session_id quit
-done
+change_dir_and_check ../
+git reset --hard && git checkout $branch_name && git pull origin $branch_name
+npm install && npm run build || exit 1
+
+# kill existing backend screen sessions
+screen -ls | awk '/\.api/ {print strtonum($1)}' | while read session_id; do screen -S "${session_id}" -X quit; done
 
 # Remove the cache
-rm -rf /var/cache/nginx/backend/*
+sudo rm -rf /var/cache/nginx/backend/*
 
 # Start the backend
 screen -dmS api npm run start
--- server_conf/webhooks/push/restartFrontend.sh
+++ server_conf/webhooks/push/restartFrontend.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-# define branch name
 branch_name="prod"
 
+change_dir_and_check() {
+   cd "$1" || {
+      echo "Could not change to directory $1. Exiting...";
+      exit 1;
+   }
+}
 # Start the frontend
-cd src/frontend || exit 1 # change directory to frontend folder
-git reset --hard # reset the changes
-git checkout $branch_name # checkout to the branch
-git pull origin $branch_name # pull the latest code
-npm install # install the dependencies
-npm run build || exit 1 # build the frontend
-cd dist/server || exit 1 # change directory to server folder
-# Get the list of screen sessions
-session_list=$(screen -ls | grep "frontend" | awk '{print $1}')
-# Iterate through the sessions and kill them
-for session_id in $session_list; do
-    screen -X -S $session_id quit
-done
+change_dir_and_check src/frontend
+git reset --hard && git checkout $branch_name && git pull origin $branch_name
+npm install && npm run build || exit 1
+change_dir_and_check dist/server
+
+# Kill existing frontend screen sessions
+screen -ls | awk '/\.frontend/ {print strtonum($1)}' | while read session_id; do screen -S "${session_id}" -X quit; done
 
 # Remove the cache
-rm -rf /var/cache/nginx/frontend/*
+sudo rm -rf /var/cache/nginx/frontend/*
 
 # Start the frontend
-screen -dmS frontend node entry.mjs # start the frontend
+screen -dmS frontend node entry.mjs
